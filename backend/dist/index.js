6d26f50eb12cbab6a8ad5f2ff92a1d05cec9812f
Elaine-Vargas <elainevargasreyes@gmail.com>
2025-08-11 12:59:33 -0400

    Revert "backend build fix"
    This reverts commit a5112a0db5390cbdce765d67780820f820126b23.

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -18,7 +18,7 @@ const factura_routes_1 = __importDefault(require("./routes/factura.routes"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)({ origin: process.env.FRONTEND_URL || 'http://localhost:8081' }));
+app.use((0, cors_1.default)({ origin: process.env.FRONTEND_URL || 'http://localhost:8082' }));
 app.use('/api/productos', producto_routes_1.default);
 app.use('/api/vendedores', vendedor_routes_1.default);
 app.use('/api/clientes', cliente_routes_1.default);
